33ca9fc6826d84c9df8cf5fa12a59a3ae6b1c087
mw491 <musaiw@outlook.com>
2022-12-21 21:15:23 +0300

    move api and gui into one project

--- goalkeeper_api/urls.py
+++ goalkeeper_api/urls.py
@@ -16,4 +16,4 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("goals.urls"))]
+urlpatterns = [path("admin/", admin.site.urls), path("api/", include("goals.urls"))]
